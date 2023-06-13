92260c045d85c0e90932ba132b380f031f456bb4
Tyler Goss <tygoss@yahoo.com>
2023-06-13 16:48:04 -0400

    Committing

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export function Shop() {
+export default function Shop() {
 	return (
-		<>
+		<div data-testid="shop-1">
 			<h1>Shop</h1>
 			{/* <button onClick={handleNewWorker("Reaper")}></button> */}
-		</>
+		</div>
 	);
 }
