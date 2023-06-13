9f578c8e56b8f5b368223592747dc5e04b81d42b
Jacob Roberts <jacobo0421@gmail.com>
2023-06-13 01:07:34 -0400

    Verifier process is now working.

--- src/Components/Levels/Purgatory.jsx
+++ src/Components/Levels/Purgatory.jsx
@@ -3,46 +3,37 @@ import { Reaper } from "../Workers/Purgatory/Reaper";
 import { Verifier } from "../Workers/Purgatory/Verifier";
 
 export function Purgatory() {
+
+	// ToDo: Move this value initialization to extrernal data file 
 	const [reapers, setReapers] = useState([
 		{
 			name: "1",
-			timeToComplete: 15000,
-			isIdle: true,
-			soul: {},
+			timeToComplete: 9000,
 		},
 	]);
 
+	// Todo: Move this value initialization to an external data file
 	const [verifiers, setVerifiers] = useState([
 		{
-			name: "1",
+			name: "Bob",
 			timeToComplete: 20000,
-			isIdle: true,
-			soul: {},
+			queueMax: 5,
+			queue: [],
 		},
 	]);
-
-	const [verifiersQueue, setVerifiersQueue] = useState([]);
 
 	const handleComplete = (workerType, id, soul) => {
 		switch (workerType) {
 			case "Reaper":
-				if (verifiersQueue.length >= 5) {
-					console.log(
-						"Too many in Verifiers queue. Not adding to queue"
-					);
-					break;
-				}
-				setVerifiersQueue([...verifiersQueue, soul]);
+
+				// findVerifierAndQueue Checks all verifiers for a queue that has a free space, then places soul. If there are none, soul is lost.
+				// Could possibly return true / false so that we can output different messages for each result.
+				findVerifierAndQueue(soul);
 				console.log("Purgatory - Set to Verifiers Queue");
 				break;
 
 			case "Verifier":
-				let verifierData = [...verifiers];
-
-				verifierData[id].soul = {};
-				verifierData[id].isIdle = true;
-				setVerifiers(verifierData);
-
+				const toDeciders = removeSoul(id);
 				console.log("Purgatory - Verifier Refreshed");
 				break;
 			default:
@@ -50,29 +41,40 @@ export function Purgatory() {
 		}
 	};
 
-	useEffect(() => {
+	const removeSoul = (id) => {
 		let verifierData = [...verifiers];
-		let queue = [...verifiersQueue];
+		let currentVerifier = verifierData[id];
 
-		if (queue.length == 0) {
+		const toRemove = currentVerifier.queue[0];
+
+		currentVerifier.queue = currentVerifier.queue.filter(
+			(s, index) => index !== 0
+		);
+
+		console.log(currentVerifier.queue.length);
+		setVerifiers(verifierData);
+
+		return toRemove;
+	};
+
+	const findVerifierAndQueue = (soul) => {
+		let verifierData = [...verifiers];
+
+		let availableVerifier = verifierData.find(
+			(v) => v.queue.length < v.queueMax
+		);
+
+		if (!availableVerifier) {
+			console.log("All Queues Filled. This soul is doomed to permanently roam the earth.");
 			return;
 		}
 
-		let verifiersSoul;
+		availableVerifier.queue = [...availableVerifier.queue, soul];
 
-		verifierData.forEach((v, index) => {
-			if (v.isIdle) {
-				verifiersSoul = queue.shift();
-				v.soul = verifiersSoul;
-				v.isIdle = false;
-			}
-		});
+		console.log(availableVerifier.queue.length);
 
 		setVerifiers(verifierData);
-		setVerifiersQueue(queue);
-
-		console.log(verifierData);
-	}, [verifiers]);
+	};
 
 	return (
 		<>
@@ -85,13 +87,13 @@ export function Purgatory() {
 					handleComplete={handleComplete}
 				/>
 			))}
-			<p>Verifiers Queue Length: {verifiersQueue.length}</p>
+			{/* <p>Verifiers Queue Length: {verifiersQueue.length}</p> */}
 			{verifiers.map((verifier, index) => (
 				<Verifier
 					key={index}
 					id={index}
 					timeToComplete={verifier.timeToComplete}
-					soul={verifier.soul}
+					soul={verifier.queue[0]}
 					handleComplete={handleComplete}
 				/>
 			))}
