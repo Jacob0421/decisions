8950569ec01df2c7e35491ba2e7f6141dad279dd
tabsalex <tablada.alex@yahoo.com>
2023-06-18 01:28:32 -0400

    Trying to fix demon and angel counters.

--- src/Data/DataIntialization/Purgatory.jsx
+++ src/Data/DataIntialization/Purgatory.jsx
@@ -1,19 +1,19 @@
 export const ReaperInitial = {
 	workers: 1,
-	timeToComplete: 1000,
+	timeToComplete: 1,
 	revenueGenerated: 1,
 };
 
 export const VerifierInitial = {
 	workers: 1,
-	timeToComplete: 2000,
+	timeToComplete: 2,
 	revenueGenerated: 10,
 	queueMax: 5,
 };
 
 export const DeciderInitial = {
 	workers: 1,
-	timeToComplete: 3000,
+	timeToComplete: 3,
 	percentCorrect: 50,
 	revenueGenerated: 100,
 	queueMax: 5,
