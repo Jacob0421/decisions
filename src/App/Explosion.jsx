a1ebe678d6a09495ceba23383d18bfc2ef6d9dcf
Jacob Roberts <jacobo0421@gmail.com>
2023-06-22 01:51:47 -0400

    Added Changes for successful build

--- src/App/Explosion.jsx
+++ src/App/Explosion.jsx
@@ -11,7 +11,7 @@ export default function Explosion() {
 					zIndex: 9999,
 					position: "absolute",
 				}}
-				alt=""
+				alt="explosion"
 			/>
 		</>
 	);
--- src/App/Levels/Purgatory/Workers/Decider.jsx
+++ src/App/Levels/Purgatory/Workers/Decider.jsx
@@ -45,6 +45,7 @@ export default function Decider(params) {
 		const ticker = setInterval(() => processSoul(), timeToComplete);
 
 		return () => clearInterval(ticker);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [souls]);
 
 	return (
